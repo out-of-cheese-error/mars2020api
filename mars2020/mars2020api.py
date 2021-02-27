830764e16e211be2d9fa53bbb1564d855f3acffd
Ninjani <48680156+Ninjani@users.noreply.github.com>
2021-02-27 14:47:18 +0100

    fix(API): edge cases and type conversions

--- mars2020/mars2020api.py
+++ mars2020/mars2020api.py
@@ -1,7 +1,6 @@
 import typing as ty
 from dataclasses import dataclass
 from datetime import datetime
-from enum import Enum
 from pathlib import Path
 
 import requests as rq
@@ -63,32 +62,20 @@ class Camera:
     def from_camera_dictionary(cls, camera_dictionary: dict):
         filter_name = check_none(camera_dictionary, "filter_name")
         camera_vector = check_none(camera_dictionary, "camera_vector")
-        camera_model_component_list = check_none(camera_dictionary, "camera_model_component_list")
+        camera_model_component_list = check_none(
+            camera_dictionary, "camera_model_component_list"
+        )
         camera_position = check_none(camera_dictionary, "camera_position")
         instrument = check_none(camera_dictionary, "instrument")
         camera_model_type = check_none(camera_dictionary, "camera_model_type")
-        return cls(filter_name=filter_name,
-                   camera_vector=camera_vector,
-                   camera_model_component_list=camera_model_component_list,
-                   camera_position=camera_position,
-                   instrument=instrument,
-                   camera_model_type=camera_model_type)
-
-
-class InstrumentIdentifier(Enum):
-    ZL = "Mastcam-Z left"
-    ZR = "Mastcam-Z right"
-    Other = "other"
-
-    @classmethod
-    def get_identifier(cls, code: str):
-        assert len(code) == 2
-        if code == "ZL":
-            return cls.ZL
-        elif code == "ZR":
-            return cls.ZR
-        else:
-            return cls.Other
+        return cls(
+            filter_name=filter_name,
+            camera_vector=camera_vector,
+            camera_model_component_list=camera_model_component_list,
+            camera_position=camera_position,
+            instrument=instrument,
+            camera_model_type=camera_model_type,
+        )
 
 
 @dataclass
@@ -98,26 +85,31 @@ class SpacecraftClockTime:
     spacecraft_clock_time_miliseconds: str
 
     @classmethod
-    def from_time_field(cls, time_code: str, product_code):
-        assert len(time_code) == 14 and len(product_code) == 6
-        return cls(int(time_code[:4]),
-                   time_code[4:],
-                   product_code[:3])
+    def from_time_field(cls, sol_code: str, time_code: str, product_code):
+        assert len(sol_code) == 4 and len(time_code) == 10 and len(product_code) == 6, (
+            sol_code,
+            time_code,
+            product_code,
+        )
+        return cls(int(sol_code), time_code, product_code[:3])
+
 
 @dataclass
 class VehicleLocation:
     site_location: str
     site_location_drive_position: str
 
+
 @dataclass
 class InstrumentMeta:
-    instrument_identifier: InstrumentIdentifier
+    instrument_identifier: str
     spacecraft_time: SpacecraftClockTime
     filter_number: str
     vehicle_location: VehicleLocation
+    thumbnail: bool
     sequence_id: ty.Tuple[str, str]
     product_type_identifier: str
-    focal_length: int
+    focal_length: str
     downsampling_factor: int
     jpeg_quality: str
     producer: chr
@@ -125,27 +117,49 @@ class InstrumentMeta:
 
     @classmethod
     def from_image_id(cls, image_id: str):
-        instrument_code, time_code, product_code, \
-        location_code, alternative_instrument_code,\
-        image_code = image_id.split("_")
-        instrument_identifier = InstrumentIdentifier.get_identifier(instrument_code[:2])
-        spacecraft_time = SpacecraftClockTime.from_time_field(time_code, product_code)
+        # From https://mastcamz.asu.edu/decoding-the-raw-publicly-released-mastcam-z-image-filenames/
+        lengths = [3, 4, 10, 3 + 3, 1 + 3 + 4 + 9, 3 + 1 + 2 + 1 + 2]
+        index = 0
+        codes = []
+        for length in lengths:
+            codes.append(image_id[index : index + length])
+            index += length + 1
+        (instrument_code, sol_code, time_code, product_code, code, image_code,) = codes
+        instrument_identifier = instrument_code[:2]
+        spacecraft_time = SpacecraftClockTime.from_time_field(
+            sol_code, time_code, product_code
+        )
         filter_number = instrument_code[-1]
-        focal_length = int(image_code[:3])
-        downsampling_factor = int(2**int(image_code[3]))
+        focal_length = image_code[:3]
+        downsampling_factor = int(2 ** int(image_code[3]))
         jpeg_quality = image_code[4:6]
         producer = image_code[6]
         version = image_code[-2:]
         product_type_identifier = product_code[-3:]
-        site_location = location_code[1:4]
-        site_location_drive_position = location_code[4:8]
-        sequence_id = (location_code[-3:], alternative_instrument_code)
-        return cls(instrument_identifier=instrument_identifier, spacecraft_time=spacecraft_time,
-                   filter_number=filter_number,
-                   vehicle_location=VehicleLocation(site_location, site_location_drive_position),
-                   sequence_id=sequence_id, product_type_identifier=product_type_identifier,
-                   focal_length=focal_length, downsampling_factor=downsampling_factor,
-                   jpeg_quality=jpeg_quality, producer=producer, version=version)
+
+        if code[0] == "T":
+            thumbnail = True
+        else:
+            thumbnail = False
+        site_location = code[1 : 1 + 3]
+        site_location_drive_position = code[4 : 4 + 4]
+        sequence_id = (code[8 : 8 + 4], code[8 + 4 : 8 + 4 + 5])
+        return cls(
+            instrument_identifier=instrument_identifier,
+            spacecraft_time=spacecraft_time,
+            filter_number=filter_number,
+            vehicle_location=VehicleLocation(
+                site_location, site_location_drive_position
+            ),
+            thumbnail=thumbnail,
+            sequence_id=sequence_id,
+            product_type_identifier=product_type_identifier,
+            focal_length=focal_length,
+            downsampling_factor=downsampling_factor,
+            jpeg_quality=jpeg_quality,
+            producer=producer,
+            version=version,
+        )
 
 
 @dataclass
@@ -161,32 +175,46 @@ class ImageData:
     image_id: str
     sol: ty.Union[None, int]
     sample_type: ty.Union[None, str]
-    mars_date: ty.Union[None, datetime]
+    mars_date: ty.Union[None, str]
     earth_date_utc: ty.Union[None, datetime]
     date_received_on_earth_utc: ty.Union[None, datetime]
 
     @classmethod
     def from_image_dictionary(cls, image_dictionary: dict):
         camera: Camera = Camera.from_camera_dictionary(image_dictionary["camera"])
-        extended_info: ExtendedInfo = ExtendedInfo.from_extended_info_dict(image_dictionary["extended"])
+        extended_info: ExtendedInfo = ExtendedInfo.from_extended_info_dict(
+            image_dictionary["extended"]
+        )
         image_url = str(image_dictionary["image_files"]["full_res"])
         caption = str(image_dictionary["caption"])
         dimension = extended_info.dimension
         title = image_dictionary["title"]
         image_id = image_dictionary["imageid"]
         attitude = check_none(image_dictionary, "attitude")
+        if attitude is not None:
+            attitude = tuple(map(float, attitude[1:-1].split(",")))
         sol = int(check_none(image_dictionary, "sol"))
-        mars_date = check_date(image_dictionary, "date_taken_mars")
+        mars_date = check_none(image_dictionary, "date_taken_mars")
         earth_date = check_date(image_dictionary, "date_taken_utc")
         date_received_on_earth_utc = check_date(image_dictionary, "date_received")
         sample_type = check_none(image_dictionary, "sample_type")
         instrument_metadata = InstrumentMeta.from_image_id(image_id)
-        return cls(camera_type=camera, image_url=image_url,
-                   attitude=attitude, dimension=dimension, caption=caption,
-                   title=title, image_id=image_id, sol=sol, mars_date=mars_date,
-                   earth_date_utc=earth_date, extended_info=extended_info,
-                   date_received_on_earth_utc=date_received_on_earth_utc,
-                   sample_type=sample_type, instrument_metadata=instrument_metadata)
+        return cls(
+            camera_type=camera,
+            image_url=image_url,
+            attitude=attitude,
+            dimension=dimension,
+            caption=caption,
+            title=title,
+            image_id=image_id,
+            sol=sol,
+            mars_date=mars_date,
+            earth_date_utc=earth_date,
+            extended_info=extended_info,
+            date_received_on_earth_utc=date_received_on_earth_utc,
+            sample_type=sample_type,
+            instrument_metadata=instrument_metadata,
+        )
 
     @property
     def image_data(self):
@@ -194,9 +222,7 @@ class ImageData:
         if not tmp_folder.exists():
             tmp_folder.mkdir()
         with open(tmp_folder / "nasa_image_temp.png", "wb") as temp_file:
-            temp_file.write(
-                rq.get(self.image_url).content
-            )
+            temp_file.write(rq.get(self.image_url).content)
         return Image.open(tmp_folder / "nasa_image_temp.png")
 
 
@@ -210,9 +236,13 @@ class ImageDataCollection:
     @classmethod
     def fetch_all_mars2020_imagedata(cls) -> "ImageDataCollection":
         all_data = cls.empty()
-        total: int = cls.fetch_partial_mars2020_imagedata(number_of_images=1, page_number=1).total_images_in_database
+        total: int = cls.fetch_partial_mars2020_imagedata(
+            number_of_images=1, page_number=1
+        ).total_images_in_database
         for step in range(int(total / 100) + 1):
-            all_data += cls.fetch_partial_mars2020_imagedata(number_of_images=100, page_number=step)
+            all_data += cls.fetch_partial_mars2020_imagedata(
+                number_of_images=100, page_number=step
+            )
         return all_data
 
     @classmethod
@@ -220,10 +250,15 @@ class ImageDataCollection:
         return cls([], None, 0, 0)
 
     @classmethod
-    def fetch_partial_mars2020_imagedata(cls, number_of_images: int, page_number: int) -> "ImageDataCollection":
+    def fetch_partial_mars2020_imagedata(
+        cls, number_of_images: int, page_number: int
+    ) -> "ImageDataCollection":
         json_data = rq.get(
-            f"https://mars.nasa.gov/rss/api/?feed=raw_images&category=mars2020&feedtype=json&num={number_of_images}&page={page_number}").json()
-        images: ty.List[ImageData] = [ImageData.from_image_dictionary(x) for x in json_data["images"]]
+            f"https://mars.nasa.gov/rss/api/?feed=raw_images&category=mars2020&feedtype=json&num={number_of_images}&page={page_number}"
+        ).json()
+        images: ty.List[ImageData] = [
+            ImageData.from_image_dictionary(x) for x in json_data["images"]
+        ]
         total_images_in_database = json_data["total_results"]
         return cls(images, page_number, number_of_images, total_images_in_database)
 
@@ -236,7 +271,7 @@ class ImageDataCollection:
             images=self.images + other.images,
             page_number=None,
             number_of_images=self.number_of_images + other.number_of_images,
-            total_images_in_database=other.total_images_in_database
+            total_images_in_database=other.total_images_in_database,
         )
 
     def __len__(self):
