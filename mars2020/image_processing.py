b6ed102119ad82af046d50fc3a5443ac87bc26d3
Ninjani <48680156+Ninjani@users.noreply.github.com>
2021-03-03 12:25:11 +0100

    refactor(bayer): different debayering algorithm

--- mars2020/image_processing.py
+++ mars2020/image_processing.py
@@ -1,55 +1,47 @@
 import numpy as np
 from PIL import Image
-import typing as ty
-from scipy.ndimage import morphology
+from scipy.ndimage.filters import convolve
 
+def demosaic_image(pil_image: Image, pattern='RGGB'):
+    """
+    Returns the demosaiced *RGB* colourspace array from given *Bayer* CFA using
+    bilinear interpolation.
+    Parameters
+    ----------
+    CFA : array_like
+        *Bayer* CFA.
+    pattern : unicode, optional
+        **{'RGGB', 'BGGR', 'GRBG', 'GBRG'}**,
+        Arrangement of the colour filters on the pixel array.
+    Returns
+    -------
+    ndarray
+        *RGB* colourspace array.
+    """
+    image: np.ndarray = np.array(pil_image, dtype=np.float64).mean(axis=2)
+    image /= np.max(image)
+    
+    pattern = pattern.upper()
 
-def red_mosaic_idx(d1: int, d2: int) -> ty.Tuple[np.ndarray, np.ndarray]:
-    red_d1: ty.List[int] = list()
-    red_d2: ty.List[int] = list()
-    for i in range(d1):
-        red_d2 += [j for j in range(0, d2, 2)]
-        red_d1 += [i] * (d2 // 2)
-    return np.array(red_d1), np.array(red_d2)
+    channels = dict((channel, np.zeros(image.shape)) for channel in 'RGB')
+    for channel, (y, x) in zip(pattern, [(0, 0), (0, 1), (1, 0), (1, 1)]):
+        channels[channel][y::2, x::2] = 1
 
+    R_m, G_m, B_m = tuple(channels[c].astype(bool) for c in 'RGB')
 
-def green_mosaic_idx(d1: int, d2: int) -> ty.Tuple[np.ndarray, np.ndarray]:
-    green_d1: ty.List[int] = list()
-    green_d2: ty.List[int] = list()
-    for i in range(d1):
-        if (i % 2) == 0:
-            green_d1 += [i] * (d2 // 2)
-            green_d2 += [j for j in range(1, d2, 2)]
-        else:
-            continue
-    return np.array(green_d1), np.array(green_d2)
+    H_G = np.array(
+        [[0, 1, 0],
+         [1, 4, 1],
+         [0, 1, 0]], dtype=np.float64) / 4  # yapf: disable
 
+    H_RB = np.array(
+        [[1, 2, 1],
+         [2, 4, 2],
+         [1, 2, 1]], dtype=np.float64) / 4  # yapf: disable
 
-def blue_mosaic_idx(d1: int, d2: int) -> ty.Tuple[np.ndarray, np.ndarray]:
-    blue_d1: ty.List[int] = list()
-    blue_d2: ty.List[int] = list()
-    for i in range(d1):
-        if (i % 2) != 0:
-            blue_d1 += [i] * (d2//2)
-            blue_d2 += [j for j in range(1, d2, 2)]
-        else:
-            continue
-    return np.array(blue_d1), np.array(blue_d2)
-
-
-def demosaic_image(pil_image: Image) -> Image:
-    image: np.ndarray = np.array(pil_image)
-    if len(image.shape) == 3:
-        image: np.ndarray = image.astype(float).mean(axis=2).astype("uint8")
-    d1, d2 = image.shape[:2]
-    red_d1, red_d2 = red_mosaic_idx(d1, d2)
-    green_d1, green_d2 = green_mosaic_idx(d1, d2)
-    blue_d1, blue_d2 = blue_mosaic_idx(d1, d2)
-    new_image = np.zeros((d1, d2, 3), dtype="uint8")
-    new_image[red_d1, red_d2, 0] = image[red_d1, red_d2]
-    new_image[green_d1, green_d2, 1] = image[green_d1, green_d2]
-    new_image[blue_d1, blue_d2, 2] = image[blue_d1, blue_d2]
-    new_image[:, :, 0] = morphology.grey_dilation(new_image[:, :, 0], size=3)
-    new_image[:, :, 1] = morphology.grey_dilation(new_image[:, :, 1], size=3)
-    new_image[:, :, 2] = morphology.grey_dilation(new_image[:, :, 2], size=3)
-    return Image.fromarray(new_image)
+    R = convolve(image * R_m, H_RB)
+    G = convolve(image * G_m, H_G)
+    B = convolve(image * B_m, H_RB)
+    
+    output = np.concatenate([x[..., np.newaxis] for x in np.array([R, G, B], dtype=np.float64)], axis=-1)
+    return Image.fromarray((output * 255).astype(np.uint8), "RGB")
