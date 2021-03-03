2d8db55fe0d3143773e4cfa3d58658bd6680714b
akdel <mehmet_akdel@hotmail.com>
2021-03-03 15:36:37 +0100

    cli added to construt grid images

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='mars2020',
       version='1.0',
       description='An unofficial api',
       author='out-of-cheese-error',
+      scripts=["tools/construct_16x16_grid_image"],
       packages=['mars2020'],
-      install_requires=["numpy", "requests", "pillow"])
+      install_requires=["numpy", "requests", "pillow", "scipy", "fire"])
