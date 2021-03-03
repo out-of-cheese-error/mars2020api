d74823e205c231457df6dc22cebbcece42cf2bf8
Ninjani <48680156+Ninjani@users.noreply.github.com>
2021-03-03 12:34:11 +0100

    style(black): formatting

--- mars2020/image_processing.py
+++ mars2020/image_processing.py
@@ -2,7 +2,8 @@ import numpy as np
 from PIL import Image
 from scipy.ndimage.filters import convolve
 
-def demosaic_image(pil_image: Image, pattern='RGGB'):
+
+def demosaic_image(pil_image: Image, pattern="RGGB"):
     """
     Returns the demosaiced *RGB* colourspace array from given *Bayer* CFA using
     bilinear interpolation.
@@ -19,29 +20,27 @@ def demosaic_image(pil_image: Image, pattern='RGGB'):
         *RGB* colourspace array.
     """
     image: np.ndarray = np.array(pil_image, dtype=np.float64).mean(axis=2)
-    image /= 255.
-    
-    pattern = pattern.upper()
+    image /= 255.0
 
-    channels = dict((channel, np.zeros(image.shape)) for channel in 'RGB')
-    for channel, (y, x) in zip(pattern, [(0, 0), (0, 1), (1, 0), (1, 1)]):
+    channels = dict((channel, np.zeros(image.shape)) for channel in "RGB")
+    for channel, (y, x) in zip(pattern.upper(), [(0, 0), (0, 1), (1, 0), (1, 1)]):
         channels[channel][y::2, x::2] = 1
 
-    R_m, G_m, B_m = tuple(channels[c].astype(bool) for c in 'RGB')
+    R_m, G_m, B_m = tuple(channels[c].astype(bool) for c in "RGB")
 
-    H_G = np.array(
-        [[0, 1, 0],
-         [1, 4, 1],
-         [0, 1, 0]], dtype=np.float64) / 4  # yapf: disable
+    H_G = (
+        np.array([[0, 1, 0], [1, 4, 1], [0, 1, 0]], dtype=np.float64) / 4
+    )  # yapf: disable
 
-    H_RB = np.array(
-        [[1, 2, 1],
-         [2, 4, 2],
-         [1, 2, 1]], dtype=np.float64) / 4  # yapf: disable
+    H_RB = (
+        np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.float64) / 4
+    )  # yapf: disable
 
     R = convolve(image * R_m, H_RB)
     G = convolve(image * G_m, H_G)
     B = convolve(image * B_m, H_RB)
-    
-    output = np.concatenate([x[..., np.newaxis] for x in np.array([R, G, B], dtype=np.float64)], axis=-1)
+
+    output = np.concatenate(
+        [x[..., np.newaxis] for x in np.array([R, G, B], dtype=np.float64)], axis=-1
+    )
     return Image.fromarray((output * 255).astype(np.uint8), "RGB")
